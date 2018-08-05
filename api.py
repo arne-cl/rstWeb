5e4f066cb48709fc799f01cd9dbfa7e48de1fc4b
Arne Neumann <nlpbox.programming@arne.cl>
2018-08-05 23:45:00 +0200

    REST API: cleanup and fixes (POST a document)

--- api.py
+++ api.py
@@ -14,18 +14,18 @@ import base64
 from collections import defaultdict
 import json
 import os
+from tempfile import mkdtemp
 
 import cherrypy
 
-from modules.rstweb_sql import (
-    create_project, delete_project, generic_query, get_all_projects, import_document)
+from modules import rstweb_sql
 from quick_export import quickexp_main
 from screenshot import get_png
 
 
 def get_all_docs(user, project):
     """Returns a list of all documents of the given user in the given project."""
-    return [elem[0] for elem in generic_query("SELECT doc FROM docs WHERE user=? AND project=?", (user, project))]
+    return [elem[0] for elem in rstweb_sql.generic_query("SELECT doc FROM docs WHERE user=? AND project=?", (user, project))]
 
 def get_screenshot(file_name, project_name, user, output_format='png'):
     """Produces a screenshot of the rhetorical structure tree of a document from
@@ -67,7 +67,6 @@ def edit_document(file_name, project_name):
     url_params = kwargs2urlparams(kwargs)
     raise cherrypy.HTTPRedirect('/structure?{0}'.format(url_params))
 
-
 def kwargs2urlparams(kwargs):
     """Converts a key-value dict into a string of URL parameters. Dict entries
     with empty values will be ignored. This is used when redirecting API requests
@@ -83,6 +82,29 @@ def kwargs2urlparams(kwargs):
 
 class APIController(object):
     """REST API for rstWeb"""
+    def __init__(self):
+        # create a temporary folder for importing files uploaded via the API
+        self.import_dir = mkdtemp()
+
+    def import_rs3_file(self, rs3_file, file_name, project_name):
+        """
+        rs3_file: cherrypy._cpreqbody.Part
+        """
+        # upload the POSTed file into the import directory
+        file_content = rs3_file.file.read()
+        import_filepath = os.path.join(self.import_dir, file_name)
+        with open(import_filepath, 'w') as import_file:
+            import_file.write(file_content)
+
+        # create project if it doesn't exist yet
+        if project_name not in self.get_projects():
+            self.add_project(project_name)
+
+        # import the file into the database, then remove the temporary file
+        error = rstweb_sql.import_document(import_filepath, project_name, 'local')
+        os.remove(import_file.name)
+        return error
+
     @cherrypy.expose
     def get_index(self):
         """Handler for / (GET)
@@ -100,7 +122,7 @@ class APIController(object):
         """Handler for /projects (GET).
         Returns a list of all projects.
         """
-        return {'projects': [elem[0] for elem in get_all_projects()]}
+        return [elem[0] for elem in rstweb_sql.get_all_projects()]
 
     @cherrypy.expose
     def delete_projects(self):
@@ -110,10 +132,16 @@ class APIController(object):
         for project in self.get_projects():
             rstweb_sql.delete_project(project)
 
+        projects = self.get_projects()
+        if projects:
+            raise cherrypy.HTTPError(
+                500, ("Could not delete all projects. Remaining projects: '{0}'.").format(projects))
+
     @cherrypy.tools.json_out()
     def get_project(self, project_name):
         """Handler for /projects/{project_name} (GET).
-        Returns a list of all documents in a project of the user 'local'.
+        Returns metadata about a project (incl. a list of all documents in that project
+        of the user 'local').
         """
         return {'documents': get_all_docs('local', project_name)}
 
@@ -125,6 +153,9 @@ class APIController(object):
         """
         rstweb_sql.create_project(project_name)
 
+        if project_name not in self.get_projects():
+            raise cherrypy.HTTPError(500, "Could not add project '{0}'".format(project_name))
+
     @cherrypy.expose
     def delete_project(self, project_name):
         """Handler for /projects/{project_name} (DELETE).
@@ -134,22 +165,34 @@ class APIController(object):
         """
         rstweb_sql.delete_project(project_name)
 
+        if project_name in self.get_projects():
+            raise cherrypy.HTTPError(500, "Could not delete project '{0}'".format(project_name))
+
     @cherrypy.tools.json_out()
     def get_documents(self):
         """Handler for /documents (GET).
-        Returns a JSON struct containing all documents in all projects of the
+        Returns a JSON struct containing all documents in all projects from the
         user 'local'.
         """
-        all_documents = generic_query("SELECT doc, project FROM docs WHERE user=?", ('local',))
+        all_documents = rstweb_sql.generic_query("SELECT doc, project FROM docs WHERE user=?", ('local',))
         docs_dict = defaultdict(list)
         for file_name, project_name in all_documents:
             docs_dict[project_name].append(file_name)
         return {'documents': docs_dict}
 
+    @cherrypy.tools.json_out()
+    def get_project_documents(self, project_name):
+        """Handler for /documents/{project_name} (GET).
+        Returns a list of all documents in that project from the user 'local').
+        """
+        return get_all_docs('local', project_name)
+
     @cherrypy.expose
     def get_document(self, project_name, file_name, output='rs3'):
         """Handler for /documents/{project_name}/{file_name} (GET)
-        TODO: how to get URL params: ?output={rs3, png, png-base64, editor}"""
+        Returns a document either as an `rs3` file, a `png` image of an RST tree,
+        a base64-encoded png image or opens it in the structure editor.
+        """
         # only proceed if the project and file exist (for the user 'local')
         documents = self.get_project(project_name).get('documents')
         if documents is None or file_name not in documents:
@@ -170,7 +213,7 @@ class APIController(object):
                 400, 'Unknown output format: {0}'.format(output))
 
     @cherrypy.expose
-    def add_document(self, project_name, file_name):
+    def add_document(self, project_name, file_name, rs3_file):
         """Handler for /documents/{project_name}/{file_name} (POST)
         Adds a new document to the given project of the user 'local'.
 
@@ -180,83 +223,55 @@ class APIController(object):
         Adding a document under a file_name (that already exists in the given
         project for the user 'local') will also raise an error. To update
         an existing document, use the `update_document` method.
+
+        Usage example:
+
+        To upload the document 'source.rs3' into the project 'my-project' and
+        store it under the name 'target.rs3', run this command in your shell.
+
+            curl -XPOST http://localhost:8080/documents/my-project/target.rs3 -F rs3_file=@source.rs3
         """
-        raise NotImplementedError
+        # do not overwrite existing document with the same file name
+        project_docs = self.get_project_documents(project_name)
+        if file_name in project_docs:
+            raise cherrypy.HTTPError(
+                500, (("File '{0}' already exists in project '{1}'. "
+                       "Use PUT to overwrite it.")).format(file_name, project_name))
+
+        # import rs3 file
+        error = self.import_rs3_file(rs3_file, file_name, project_name)
+
+        # check if document was imported
+        project_docs = self.get_project_documents(project_name)
+        if error is not None or file_name not in project_docs:
+            raise cherrypy.HTTPError(
+                500, ("Cannot import document into project '{0}' with "
+                      "filename '{1}'. Reason: '{2}'").format(project, file_name, error))
 
     @cherrypy.expose
-    def update_document(self, project_name, file_name):
+    def update_document(self, project_name, file_name, rs3_file):
         """Handler for /documents/{project_name}/{file_name} (PUT)
         Updates a document in the given project of the user 'local'.
 
         Updating a non-existing document is the same as adding a new document.
         """
-        raise NotImplementedError
+        # import rs3 file
+        error = self.import_rs3_file(rs3_file, file_name, project_name)
 
-
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def add_node(self): \
-            # pylint: disable=no-self-use
-        """
-        Handler for /nodes (POST)
-        """
-
-        request_data = cherrypy.request.json
-
-        data, errors = NodeSchema().load(request_data)
-
-        if errors:
-            # Attempt to format errors dict from Marshmallow
-            errmsg = ', '.join(
-                ['Key: [{0}], Error: {1}'.format(key, error)
-                 for key, error in errors.items()])
-
+        # check if document exists
+        project_docs = self.get_project_documents(project_name)
+        if error is not None or file_name not in project_docs:
             raise cherrypy.HTTPError(
-                400, 'Malformed POST request data: {0}'.format(errmsg))
-
-        # Successful POST request
-        return 'TODO: add node [{0}]'.format(data['name'])
-
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def update_node(self, name): \
-            # pylint: disable=no-self-use
-        """
-        Handler for /nodes/<name> (PUT)
-        """
-
-        if name not in sample_nodes:
-            raise cherrypy.HTTPError(
-                404, 'Node \"{0}\" not found'.format(name))
-
-        # Empty response (http status 204) for successful PUT request
-        cherrypy.response.status = 204
-
-        return ''
-
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def delete_node(self, name): \
-            # pylint: disable=unused-argument,no-self-use
-        """
-        Handler for /nodes/<name> (DELETE)
-        """
-
-        # TODO: handle DELETE here
-
-        # Empty response (http status 204) for successful DELETE request
-        cherrypy.response.status = 204
-
-        return ''
+                500, ("Cannot update document '{0}' in project '{1}' "
+                      "Reason: '{2}'").format(file_name, project_name, error))
+        else:
+            return "Updated document '{0}' in project '{1}'".format(project_name, file_name)
 
 
-def jsonify_error(status, message, traceback, version): \
-        # pylint: disable=unused-argument
-
+def jsonify_error(status, message, traceback, version):
     """JSONify all CherryPy error responses (created by raising the
     cherrypy.HTTPError exception)
     """
-
     cherrypy.response.headers['Content-Type'] = 'application/json'
     response_body = json.dumps(
         {
@@ -267,7 +282,6 @@ def jsonify_error(status, message, traceback, version): \
         })
 
     cherrypy.response.status = status
-
     return response_body
 
 
@@ -296,8 +310,6 @@ def create_api_dispatcher():
                        conditions={'method': ['DELETE']})
 
     # /projects/{project_name} (GET)
-    #
-    # Request "/projects/notfound" (GET) to test the 404 (not found) handler
     dispatcher.connect(name='projects',
                        route='/projects/{project_name}',
                        action='get_project',
@@ -326,14 +338,9 @@ def create_api_dispatcher():
                        conditions={'method': ['GET']})
 
     # /documents/{project_name} (GET)
-    #
-    # Request "/projects/notfound" (GET) to test the 404 (not found) handler
-    #
-    # Right now, /projects/{project_name} and /documents/{project_name} both use get_project().
-    # This might change if we want to show more info on a project than just its file names.
     dispatcher.connect(name='documents',
                        route='/documents/{project_name}',
-                       action='get_project',
+                       action='get_project_documents',
                        controller=APIController(),
                        conditions={'method': ['GET']})
 
@@ -343,4 +350,100 @@ def create_api_dispatcher():
                        action='get_document',
                        controller=APIController(),
                        conditions={'method': ['GET']})
+
+    # /documents/{project_name}/{file_name} (POST)
+    dispatcher.connect(name='documents',
+                       route='/documents/{project_name}/{file_name}',
+                       action='add_document',
+                       controller=APIController(),
+                       conditions={'method': ['POST']})
+
+    # /documents/{project_name}/{file_name} (PUT)
+    dispatcher.connect(name='documents',
+                       route='/documents/{project_name}/{file_name}',
+                       action='update_document',
+                       controller=APIController(),
+                       conditions={'method': ['PUT']})
+
     return dispatcher
+
+
+def _merge_configs(config, section_name, newconfig):
+    """Merge a configuration section inside
+    a previous config (file, dict, reprconf.Config).
+    """
+    if config is None:
+        config = reprconf.Config()
+    else:
+        config = reprconf.Config(config)
+    section = config.get(section_name, {})
+    section.update(newconfig)
+    config[section_name] = section
+    return config
+
+def run_with_debugger(app, config=None, autoreload=True, def_host='0.0.0.0',
+                      def_port=8080):
+    """Run the cherrypy application with the CherryPyWSGIServer and with the
+    werkzeug debugging middleware.
+
+    The WSGI server is wrapped inside a ServerAdapter and subscribed to the
+    cherrypy.engine, so any cherrypy plugin is going to work with this.
+
+    By default the Autoreloader plugin is enabled, this obeys the
+    *autoreload* parameter.
+
+    **The wrapped applicantion cannot use the  InternalRedirect exception.**
+    """
+    dbgconfig = {
+          'request.throw_errors': True,
+          'wsgi.pipeline': [('debugger', DebuggedApplication),],
+          'wsgi.debugger.evalex': True
+    }
+    config = _merge_configs(config, '/', dbgconfig)
+    cherrypy.config.update(config)
+    if 'global' in config:
+        host = config['global'].get('server.socket_host', def_host)
+        port = config['global'].get('server.socket_port', def_port)
+    else:
+        host, port = def_host, def_port
+    bind_addr = (host, port)
+    app = cherrypy.Application(app, None, config=config)
+    wserver = CherryPyWSGIServer(bind_addr, app)
+    cherrypy.server.unsubscribe()
+    # bind_addr is not really required in ServerAdapter, but it
+    # does improve the messages that generates the adapter.
+    ServerAdapter(cherrypy.engine, wserver, bind_addr).subscribe()
+    if autoreload:
+        Autoreloader(cherrypy.engine).subscribe()
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+
+if __name__ == '__main__':
+    import sys
+
+    import cherrypy
+    from cherrypy.lib import reprconf
+    from cheroot.wsgi import Server as CherryPyWSGIServer
+    from cherrypy.process.plugins import Autoreloader
+    from cherrypy.process.servers import ServerAdapter
+    from werkzeug.debug import DebuggedApplication
+
+    import cherrypy_cors
+    cherrypy_cors.install()
+
+    dispatcher = create_api_dispatcher()
+    current_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
+
+    api_conf = {
+            '/': {
+                'request.dispatch': dispatcher,
+                'error_page.default': jsonify_error,
+                'cors.expose.on': True,
+            },
+            '/css': {'tools.staticdir.on': True,'tools.staticdir.dir': os.path.join(current_dir,'css')},
+            '/img': {'tools.staticdir.on': True,'tools.staticdir.dir': os.path.join(current_dir,'img')},
+            '/script': {'tools.staticdir.on': True,'tools.staticdir.dir': os.path.join(current_dir,'script')}
+    }
+
+    run_with_debugger(APIController(), api_conf)
